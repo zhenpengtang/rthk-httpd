76f69c055a415483154944fe0c9e7fc00efb438e
zhenpengtang <tangzhenpeng@gmail.com>
2013-08-18 02:23:02 +0000

    new file:   __init__.py 	new file:   radio.py

---
+++ handle/radio.py
@@ -0,0 +1,31 @@
+
+import os
+
+command_on=('supervisorctl start hkradio',)
+command_off=('supervisorctl stop hkradio',
+             'killall mplayer',)
+
+volume_run=('amixer sset \'PCM\' ')
+
+def myrun(command):
+    for i in command:
+        print i
+        os.system(i)
+
+def radioOFF():
+    print 'Radio OFF'
+    myrun(command_off)
+
+def radioON():
+    print 'Radio ON'
+    myrun(command_on)
+
+def volumeSet(vol):
+    print volume_run+vol
+    os.system(volume_run+vol)
+
+
+if __name__=='__main__':
+    radioOFF()
+
+
